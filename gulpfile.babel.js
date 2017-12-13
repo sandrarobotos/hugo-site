2b330f8cfde90cc3f4df85667567f8c9c5f2ccec
Sandra Robotos <sandrar@ecobee.com>
2017-12-13 10:05:53 -0500

    🔧 Updated css task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,7 +25,7 @@ gulp.task("build-preview", ["css", "js"], (cb) => buildSite(cb, hugoArgsPreview,
 
 // Compile CSS with PostCSS
 gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
+  gulp.src("./src/css/**/*.css")
     .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
